2604fa680ca957e13e1a4a802e9a4b6a5c2d7763
Areeb <areeb.hw@gmail.com>
2024-04-01 13:59:15 +0500

    Website layout and nav

--- resources/views/components/website/website-layout.blade.php
+++ resources/views/components/website/website-layout.blade.php
@@ -8,7 +8,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <link rel="prefetch" href="{{ route('website.home') }}">
+        <link rel="prefetch" href="{{ route('website.index') }}">
         <link rel="prefetch" href="{{ route('website.about') }}">
 
         <!-- Scripts -->
