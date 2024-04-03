7f2cb3a062325977df1f0c6a3f5389b39c83e55f
Areeb <areeb.hw@gmail.com>
2024-04-04 02:33:04 +0500

    Website layout and nav

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +12,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
@@ -67,5 +69,10 @@ class User extends Authenticatable
     {
         $hash = md5(strtolower(trim($this->email)));
         return "https://www.gravatar.com/avatar/$hash?d=mp&s=200";
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@firnaas.com') && $this->hasVerifiedEmail();
     }
 }
